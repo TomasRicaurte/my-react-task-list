8120bea3857f4567f0146fa7d259061dcc6c9c41
Tomas Ricaurte <tomiychiqui@gmail.com>
2023-10-24 18:57:47 -0500

    React puro para las tareas

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,23 @@ import './App.css';
 function App() {
 
   return (
-    <div>
-      
+  <>   
+    <div className='Header'>
+      <h1>ToDo App</h1>
     </div>
+    <div className='TaskList'>
+      <ul className='Task'>
+        <li>
+          <h4>Comprar un nuevo computador</h4>
+          <button>Completo</button> <button> incompleto</button>
+        </li>
+        <li>
+          <h4>Mandar a reparar un viejo computador</h4>
+          <button>Completo</button> <button>Incompleto</button>
+        </li>
+      </ul>
+    </div>
+  </> 
   );
 }
 
