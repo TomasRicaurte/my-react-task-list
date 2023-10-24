29dd30137aa060a95e3128000ee209582c559854
Tomas Ricaurte <tomiychiqui@gmail.com>
2023-10-24 18:27:33 -0500

    Lista React base

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import './App.css';
-import Header  from './Header';
-import TaskList from './TaskList';
 
 function App() {
-  const tasks = [
-    { id: 1, name: "Tarea 1", completed: true },
-    { id: 2, name: "Tarea 2", completed: false }
-  ];
 
   return (
     <div>
-      <Header title="ToDo App" />
-      <TaskList tasks={tasks} />
+      
     </div>
   );
 }
